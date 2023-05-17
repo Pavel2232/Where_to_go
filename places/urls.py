e041c4f3fe2593a97888621000de7999e14bde56
Павел <king-he@mail.ru>
2023-05-17 10:50:13 +0300

    danger: don't work front

--- places/urls.py
+++ places/urls.py
@@ -1,8 +1,9 @@
-# from django.contrib import admin
-# from django.urls import path
-#
-# from places import views
-#
-# urlpatterns = [
-#     path('/', views.index),
-# ]
+from django.contrib import admin
+from django.urls import path
+
+from places import views
+
+urlpatterns = [
+    path('', views.index),
+    path('<int:pk>/', views.detail_view, name='detail_view'),
+]
--- places/views.py
+++ places/views.py
@@ -1,21 +1,59 @@
+import copy
+
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-
-from places.models import Place
-
+from django.urls import reverse
+from places.models import Place, Image
+from django.http import JsonResponse
 
 # Create your views here.
 @csrf_exempt
 def index(request):
+    features = []
+    feature_template = {
+        "type": "Feature",
+        "geometry": {"type": "Point", "coordinates": [1, 1]},
+        "properties": {
+            "title": "title",
+            "placeId": "placeId",
+            "detailsUrl": "detailsUrl",
+        },
+    }
+
     places = Place.objects.all()
-    response = []
+
     for place in places:
-        response.append({
-            'id': place.id,
-            'title': place.title,
-            'description_short': place.description_short,
-            'description_long': place.description_long,
-            'coordinates': [place.lng, place.lat],
-            'image': place.image
-        })
-    return (render(request,'index.html', context = {'response':response}))
+        new_feature = copy.deepcopy(feature_template)
+        new_feature["geometry"]["coordinates"][0] = place.lng
+        new_feature["geometry"]["coordinates"][1] = place.lat
+        new_feature["properties"]["title"] = place.title
+        new_feature["properties"]["placeId"] = "placeId" + str(place.id)
+        new_feature["properties"]["detailsUrl"] = reverse(
+            "detail_view", kwargs={"pk": place.id}
+        )
+        features.append(new_feature)
+
+    places_data = {"type": "FeatureCollection", "features": features}
+    context = {"places_data": places_data}
+    return render(request, "index.html", context )
+
+
+def detail_view(request, pk):
+    place = Place.objects.get(pk=pk)
+
+    response = {
+        'title': place.title,
+        'imgs': [],
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lat': place.lat,
+            'lng': place.lng
+        }
+    }
+    imgs = Image.objects.filter(places=place.id)
+    for image in imgs:
+        response['imgs'].append(image.imgs.url)
+
+
+    return JsonResponse(response)
