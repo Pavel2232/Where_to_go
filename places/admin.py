fe730c0203c4eb95a3b6f4edf2983d81ebe56686
Павел <king-he@mail.ru>
2023-05-19 00:31:04 +0300

    feat:add a photo via the place admin panel

--- places/admin.py
+++ places/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
+from django.contrib.admin import register
 
 from places.models import Place, Image
 
+class ImageInline(admin.TabularInline):
+    model = Image
+@register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInline,
+    ]
+
+
 # Register your models here.
-admin.site.register(Place)
+# admin.site.register(Place)
 admin.site.register(Image)
+
