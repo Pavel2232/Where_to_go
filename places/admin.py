1a934a93652b2da6103e72a2276a92da9963a3c9
Павел <king-he@mail.ru>
2023-05-20 17:46:47 +0300

    feat:add a photo via the place admin panel with images in admin panel

--- places/admin.py
+++ places/admin.py
@@ -1,15 +1,35 @@
 from django.contrib import admin
 from django.contrib.admin import register
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 from places.models import Place, Image
-
+from django.urls import reverse
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ['image_link']
+    fields = ('imgs', 'image_link', 'number_image',)
+
+    def image_link(self, obj):
+        try:
+            return mark_safe('<img src = "{url}" width= "{width}" height={height} />'.format(
+                url=obj.imgs.url,
+                width=200,
+                height=200,
+            )
+        )
+
+        except Exception as e:
+            print(e)
+    image_link.short_description = "Превью"
 @register(Place)
 class PlaceAdmin(admin.ModelAdmin):
+
     inlines = [
         ImageInline,
     ]
+
+
 
 
 # Register your models here.
--- places/tests.py
+++ places/tests.py
@@ -1,3 +1,3 @@
-from django.test import TestCase
+from places.models import Place
 
-# Create your tests here.
+print(Place.image)
