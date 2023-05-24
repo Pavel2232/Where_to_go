92ef209c65aaeb7124ba1b7be057f619c3e302d4
Pavel2232 <106510078+Pavel2232@users.noreply.github.com>
2023-05-24 11:14:59 +0300

    Update urls.py

--- places/urls.py
+++ places/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from places import views
 
 urlpatterns = [
-    path('/', views.index),
+    path('', views.index),
     path('<int:pk>/', views.detail_view, name='detail_view'),
 ]
