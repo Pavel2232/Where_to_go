5384e99dd90b802b9d822786c728dc8819c87c02
Павел <king-he@mail.ru>
2023-05-25 16:03:25 +0300

    refactor dok-comp

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+python manage.py collectstatic
 python manage.py migrate --check
 status=$?
 if [[ $status != 0 ]]; then
-  python manage.py collectstatic
   python manage.py makemigrations
   python manage.py migrate
 fi
