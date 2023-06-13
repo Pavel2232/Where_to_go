197478acd1b93d05a53841fc838f2fb38f013e11
Павел <king-he@mail.ru>
2023-06-13 11:23:56 +0300

    refactor

--- places/migrations/0002_alter_place_description_long.py
+++ places/migrations/0002_alter_place_description_long.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='place',
             name='description_long',
-            field=models.CharField(max_length=5000),
+            field=models.CharField(max_length=10000),
         ),
     ]
